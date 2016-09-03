bff4b59e20187b12619f34465cf7aafe5f447b1c
Luiz Gustavo <lglmoura@gmail.com>
2016-09-03 16:35:46 -0300

    Criando Activity Contato

---
+++ app/src/main/java/br/edu/iff/pooa20161/contatos/activity/ContatoActivity.java
@@ -0,0 +1,15 @@
+package br.edu.iff.pooa20161.contatos.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.iff.pooa20161.contatos.R;
+
+public class ContatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+    }
+}
